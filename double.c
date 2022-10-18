63d5ca12d68b23138fad555f7dcdb2f7e063c552
orun sunny <sunny.mukut@gmail.com>
2022-10-18 15:55:09 +0600

    added

---
+++ double.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+     int num1;
+     printf("Enter a number ");
+     scanf("%d",&num1);
+     printf("%d",num1*2);
+     return 0;
+
+}
--- simpleText.c
+++ simpleText.c
@@ -1,1 +1,7 @@
+#include <stdio.h>
+int main()
+{
+    printf("Hi there /\.       I hope you are doing great and practicing well.\nNow this is a simple task you need to do.    Just care for the indentation.\nShow 100%% of sincerity ");
 
+    return 0;
+}
