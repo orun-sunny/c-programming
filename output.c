b161788e9534ce6afbd5662ce2f088e3a6034e06
orun sunny <sunny.mukut@gmail.com>
2022-10-31 01:13:46 +0600

    addedAll

--- output.c
+++ output.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
-
 int main()
 {
-
-    int arr[5],i,sum=0;
-    for(i=1; i<=5; i+=1)
+    char arr[2]= {'a','b'},i,j;
+    for(i=0; i<2; i++)
     {
-        arr[i-1]=i-1;
-    }
-    for(i=0; i<5; i+=1)
-    {
-        printf("%d ",arr[i]);
+        for(j=0; j<2; j++)
+        {
+            printf("%c",arr[j]);
+        }
+        char ch=arr[0];
+        arr[0]=arr[1];
+        arr[1]=ch;
     }
     return 0;
 }
