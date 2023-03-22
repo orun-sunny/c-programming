4da376e1587abb86442858458d651f79256d5c57
orun sunny <sunny.mukut@gmail.com>
2023-03-22 23:44:33 +0600

    addedLinkedlist

---
+++ func.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//string erase_first_last(string s)
+//{
+//
+//    s.erase(s.begin());
+//    s.pop_back();
+//
+//    return s;
+//}
+int main()
+{
+    int a,b;
+    cin >>a>>b;
+    swap(a,b);
+    cout<<a<<" "<<b<<"";
+    return 0;
+
+
+
+//    string s;
+//    cin>>s;
+//    string ans = erase_first_last(s);
+//    cout<<s<<"\n";
+//    cout<<ans<<"\n";
+    return 0;
+}
---
+++ linkedlist.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+class node
+{
+public:
+    int data;
+    node * nxt ;
+};
+
+class LinkedList
+{
+    node * head;
+
+    LinkedList()
+    {
+        head =NULL;
+    }
+    void InsertAtHead()
+    {
+
+    }
+    void Traverse()
+    {
+
+    }
+    void SearchDistinctValue()
+    {
+
+    }
+    void SearchAllValue()
+    {
+
+    }
+
+
+};
+
+ int main()
+ {
+     LinkedList l;
+     l.InsertAtHead(10);
+        l.InsertAtHead(30);
+     l.InsertAtHead(20);
+     l.InsertAtHead(30);
+
+     l.Traverse();
+     l.SearchDistinctValue(10);
+     l.SearchDistinctValue(5);
+     l.SearchAllValue(10);
+
+     return 0;
+ }
