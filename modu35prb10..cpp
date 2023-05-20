ad92caf080aa8303b626c1903c76601ba41012dc
orun-sunny <sunny.mukut@gmail.com>
2023-05-20 19:15:23 +0600

    'all

--- modu35prb10..cpp
+++ modu35prb10..cpp
@@ -2,11 +2,11 @@
 using namespace std;
 
 int main() {
-   int n;
-   cin>>n;
+   int X;
+   cin>>X;
    set<int> st;
 
-   for( int i=0; i<n; i++ ) {
+   for( int i=0; i<X; i++ ) {
       int val;
       cin>>val;
       st.insert( val );
