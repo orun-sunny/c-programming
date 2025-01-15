d2d84875be560a32239d0f0ac00590fe9c54e2e7
orun-sunny <sunny.mukut@gmail.com>
2025-01-15 23:51:01 +0600

    added

---
+++ pracloop.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+int main()
+{
+    int ara[5];
+    int i;
+    for(i=0;i<5;i++)
+    {
+        scanf("%d",&ara[i]);
+    }
+    for(i=0;i<5;i++)
+    {
+        printf("%d",ara[i]);
+    }
+
+    return 0;
+}
